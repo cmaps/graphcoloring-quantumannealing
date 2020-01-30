dd06c5b9c861504d3f644cdb4214b27589149cb3
cmaps <carla.maps@gmail.com>
2020-01-30 16:48:23 +0000

    Add files via upload

---
+++ Quantum/GCcasestudy-n5-polReduction/Solutions.py
@@ -0,0 +1,64 @@
+# Script by Carla Silva 2019 :: Graph Coloring Quantum Version
+
+""" Graph Coloring Problem
+
+    Formulation of the problem for a graph G=(V,E) with a number of colors n.
+
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import sys
+import seaborn as sns
+import pandas as pd
+import csv
+import json
+import itertools
+import pandas
+from pandas.plotting import parallel_coordinates
+import seaborn as sns
+
+if __name__ == "__main__":
+    n = 5
+    solutions = []
+    sol = []
+    lfiles = ['GC5A0B130.csv','GC5A1B255.csv','GC5A2B380.csv','GC5A3B505.csv','GC5A4B630.csv','GC5A5B755.csv']
+    files = os.listdir(os.getcwd())
+    for file in lfiles:
+        arr = []
+        with open(os.getcwd()+'/'+file, 'r') as rf:
+            reader = csv.reader(rf, delimiter=',')
+            next(reader, None)
+            for row in reader:
+                jsonr = row[3].replace("'", "\"")
+                d = json.loads(jsonr)
+                l = list(d.values())
+                if row[0] == str(0.0):
+                    arr.append(l)
+        j = 0
+        for l in arr:
+            if (sum(l[0:n]) >= 3 and sum(l[n:len(l)]) >= 4): # more or equal than 3 colored nodes and 4 edges              
+                j = j + 1
+        sol.append(j) 
+    orig_stdout = sys.stdout
+    f = open('Solutions.txt', 'w')
+    sys.stdout = f
+    print("Possible Solutions: ")
+    for i in range(0,len(lfiles)):
+        print(lfiles[i])
+        print(sol[i])
+    sys.stdout = orig_stdout
+    f.close()
+
+    df = pd.DataFrame()
+    df = df.T
+    for i in range(0,len(sol)):
+        df2 = pd.DataFrame([abs(x - sol[i]) for x in sol])
+        df = pd.concat([df, df2], axis=1)
+    df.columns = [r'$\alpha$=0' '\n' r'$\beta$=130', r'$\alpha$=1' '\n' r'$\beta$=255', r'$\alpha$=2' '\n' r'$\beta$=380', r'$\alpha$=3' '\n' r'$\beta$=505', r'$\alpha$=4' '\n' r'$\beta$=630', r'$\alpha$=5' '\n' r'$\beta$=755']
+    ax=df.plot(kind='bar',  colormap='Set3', stacked=False)
+    ax.set(ylabel='Variation of possible solutions')
+    ax.set_xticklabels(df.columns, rotation=360)
+    ax.legend(title='Q(ms)',bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+    plt.savefig('groupedbar_ms.pdf', bbox_inches='tight')
---
+++ Quantum/GCcasestudy-n5-polReduction/plotEnergy.py
@@ -0,0 +1,61 @@
+# Script by Carla Silva 2019 :: Graph Coloring Quantum Version
+
+""" Graph Coloring Problem
+
+    Formulation of the problem for a graph G=(V,E) with a number of colors n.
+
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import sys
+import seaborn as sns
+import pandas as pd
+import csv
+import json
+
+if __name__ == "__main__":
+    energy = []
+    i = 0
+    n = 5
+    df = {}
+    lfiles = ['GC5A0B130.csv','GC5A1B255.csv','GC5A2B380.csv','GC5A3B505.csv','GC5A4B630.csv','GC5A5B755.csv']
+    files = os.listdir(os.getcwd())
+    for file in lfiles:
+        energy = []
+        with open(os.getcwd()+'/'+file, 'r') as rf:
+            reader = csv.reader(rf, delimiter=',')
+            next(reader, None)
+            for row in reader:
+                energy.append(float(row[1]))
+            df[i] = pd.DataFrame(energy) 
+            i = i + 1
+
+    energy = pd.concat([df[0],df[1],df[2],df[3],df[4],df[5]], axis=1)
+
+    energy.columns = [r'$\alpha$=0' '\n' r'$\beta$=130', r'$\alpha$=1' '\n' r'$\beta$=255', r'$\alpha$=2' '\n' r'$\beta$=380', r'$\alpha$=3' '\n' r'$\beta$=505', r'$\alpha$=4' '\n' r'$\beta$=630', r'$\alpha$=5' '\n' r'$\beta$=755']
+
+    orig_stdout = sys.stdout
+    f = open('Energy.txt', 'w')
+    sys.stdout = f
+
+    pd.set_option('display.max_columns', None)
+    
+    print(energy.describe())
+
+    sys.stdout = orig_stdout
+    f.close()
+
+    f = plt.figure()
+    ax = energy.boxplot(showfliers=False, grid=False, showmeans=True) #outliers removed
+    ax.set_ylabel("Energy")
+    # Hide the right and top spines
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+
+    # Only show ticks on the left and bottom spines
+    ax.yaxis.set_ticks_position('left')
+    ax.xaxis.set_ticks_position('bottom')
+
+    f.savefig('boxplotEnergy.pdf', bbox_inches='tight')
